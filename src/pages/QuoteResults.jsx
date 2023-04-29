c5899f471070464c3ccb21e73d72ed87ef8a92c2
Amjad Dahdoul <amjad.dahdoul@gmail.com>
2023-04-29 16:54:50 +0300

    Top button design fix

--- src/pages/QuoteResults.jsx
+++ src/pages/QuoteResults.jsx
@@ -34,8 +34,10 @@ const QuoteResults = () => {
         smooth
         style={{
           backgroundColor: "transparent",
-          border: "1px solid",
+          border: "3px solid",
           boxShadow: "none",
+          borderRadius: "50%",
+          padding: "2px",
         }}
         component={<FaArrowUp size={"100%"} />}
       />
