1221f5802a975041cf84086f1ebd75aad7618b72
Amjad Dahdoul <amjad.dahdoul@gmail.com>
2023-04-30 14:49:28 +0300

    fix: design fix home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,47 @@ import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className='flex justify-center gap-5 text-center font-bold text-xl m-10'>
-      <NavLink to='/' className='w-screen'>
-        <div className='rounded-lg shadow-lg p-5 '>Random Quotes</div>
+    // <div className='flex justify-center gap-5 text-center font-bold text-xl m-10'>
+    //   <NavLink to='/' className='w-screen'>
+    //     <div className='rounded-lg shadow-lg p-5 '>Random Quotes</div>
+    //   </NavLink>
+    //   <div className='divider divider-horizontal'></div>
+    //   <NavLink to='quotes' className='w-screen'>
+    //     <div className='rounded-lg shadow-lg p-5 '>Quotes</div>
+    //   </NavLink>
+    // </div>
+    <div className='flex m-5 font-bold'>
+      <NavLink
+        to='/'
+        className='grid h-20 w-screen card bg-base-200 rounded-box place-items-center'
+      >
+        Random Quotes
       </NavLink>
       <div className='divider divider-horizontal'></div>
-      <NavLink to='quotes' className='w-screen'>
-        <div className='rounded-lg shadow-lg p-5 '>Quotes</div>
+      <NavLink
+        to='quotes'
+        className='grid h-20 w-screen card bg-base-200 rounded-box place-items-center'
+      >
+        Quotes
       </NavLink>
     </div>
   );
 };
 
 export default Home;
+
+<div className='flex w-full'>
+  <NavLink
+    to='/'
+    className='grid h-20 flex-grow card bg-base-300 rounded-box place-items-center'
+  >
+    Random Quotes
+  </NavLink>
+  <div className='divider divider-horizontal'></div>
+  <NavLink
+    to='quotes'
+    className='grid h-20 flex-grow card bg-base-300 rounded-box place-items-center'
+  >
+    Quotes
+  </NavLink>
+</div>;
